20a744a3f837c5c311b35a03e5cb5062b1164058
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-04 05:41:22 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
-
 set -e
 
 EXTRACTED=b-ex
-
 if [ -d "$EXTRACTED" ]; then
 	echo "[NOTE] Directory $EXTRACTED already exists."
 else
 	# Decompile with Apktool (decode resources + classes)
 	echo "[INFO] Downloading apktool"
 	wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.12.0.jar -O apktool.jar
-
 	echo "[INFO] apktool version $(java -jar apktool.jar -version)"
-
 	echo "[INFO] Decompiling Brave browser APK"
 	java -jar apktool.jar d brave.apk -r -o $EXTRACTED
-
 	echo "[INFO] Removing META-INF directory"
 	rm -rf $EXTRACTED/META-INF
 fi
-
 pushd $EXTRACTED
-
 function patch() {
 	local name="$1"
 	echo "[INFO] Patching $name"
@@ -34,20 +27,16 @@ function patch() {
 }
 
 echo "[INFO] Patching resources"
-
 patch ../patches/0000-black.rb
 patch ../patches/0001-bottom_toolbar_swipe_up.rb
-
 popd
 
 # Recompile the APK
 echo "[INFO] Recompiling APK"
-java -jar apktool.jar b b-ex -o b-patched.apk
-
+java -jar apktool.jar b b-ex -o b-patched.apk --use-aapt2
 # Align and sign the APK
 echo "[INFO] Aligning the APK"
 zipalign -p -f 4 b-patched.apk b-signed.apk
-
 # Clean up
 rm -rf b-patched.apk
 
